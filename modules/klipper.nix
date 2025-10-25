c8171fc61131695d96be0a4bc6acc21481b4d2a8
yung-gimp <172232649+yung-gimp@users.noreply.github.com>
2025-10-24 22:55:07 -0400

    feat: auto config moonraker
    read configs directory for klipper configs and make instances based on that

--- modules/klipper.nix
+++ modules/klipper.nix
@@ -3,11 +3,49 @@
   pkgs,
   ...
 }: let
-  # Printers are defined as printer-name = ./path/to/config;
-  printers = {
-    printer1 = ./configs/printer1;
-    printer2 = ./configs/printer2;
-  };
+  # Filter ./config directory for directories containing a printer.cfg file, return a list
+  printerNames = lib.attrNames (lib.filterAttrs (name: type: (type == "directory") && ((builtins.readDir ./configs/${name}/klipper) ? "printer.cfg")) (builtins.readDir ./configs));
+
+  # Take list of printer names as input, output attribute set containing printers and paths to their configs, auto generate moonraker config because it doesn't need to change per printer
+  printers = lib.mergeAttrsList (lib.imap0 (i: printerName: {
+      ${printerName} = {
+        klipperCfg = "./configs/${printerName}/printer.cfg";
+        moonrakerCfg = mkMoonraker printerName (builtins.toString (7125 + i));
+      };
+    })
+    printerNames);
+
+  # Template moonraker config
+  mkMoonraker = printerName: port:
+    pkgs.writeText "moonraker.conf" ''
+      [server]
+      host: 0.0.0.0
+      port: ${port}
+      klippy_uds_address: /run/klipper-${printerName}/klippy_uds
+
+      [authorization]
+      trusted_clients:
+          10.0.0.0/8
+          127.0.0.0/8
+          169.254.0.0/16
+          172.16.0.0/12
+          192.168.0.0/16
+          FC00::/7
+          FE80::/10
+          ::1/128
+      cors_domains:
+          *.lan
+          *.local
+          *://localhost
+          *://localhost:*
+          *://my.mainsail.xyz
+          *://app.fluidd.xyz
+
+      [history]
+
+      [update_manager]
+      enable_system_updates: False
+    '';
 in {
   environment.systemPackages = [pkgs.klipper pkgs.moonraker];
 
@@ -81,35 +119,34 @@ in {
             StateDirectory = "data-%i";
             RuntimeDirectory = "moonraker-%i";
             WorkingDirectory = "/var/lib/data-%i";
-            ExecStart = "${lib.getExe pkgs.moonraker} -d /var/lib/data-%i -c /var/lib/data-%i/config/moonraker.conf";
             Restart = "always";
             RestartSec = 10;
           };
         };
       }
-      # Enable klipper service template unit for each definition in printers
-      // lib.mapAttrs' (printerName: _configFile:
-        lib.nameValuePair "klipper@${printerName}" {
-          wantedBy = ["multi-user.target"];
-          overrideStrategy = "asDropin";
-        })
-      printers
-      # Enable moonraker service template unit for each definition in printers
-      // lib.mapAttrs' (printerName: _configFile:
-        lib.nameValuePair "moonraker@${printerName}" {
-          wantedBy = ["multi-user.target"];
-          overrideStrategy = "asDropin";
-        })
+      # Enable klipper/moonraker service template unit for each definition in printers
+      // lib.foldlAttrs (acc: printerName: configs:
+        acc
+        // {
+          "klipper@${printerName}" = {
+            wantedBy = ["multi-user.target"];
+            overrideStrategy = "asDropin";
+          };
+          "moonraker@${printerName}" = {
+            wantedBy = ["multi-user.target"];
+            overrideStrategy = "asDropin";
+            serviceConfig.ExecStart = "${lib.getExe pkgs.moonraker} -d /var/lib/data-${printerName} -c ${configs.moonrakerCfg}";
+          };
+        }) {}
       printers;
 
     # Copy each printer.cfg to the from the nix store to /var/lib so it is writeable by klipper
-    tmpfiles.rules = lib.flatten (lib.mapAttrsToList (printerName: configFile: [
+    tmpfiles.rules = lib.flatten (lib.mapAttrsToList (printerName: configs: [
         "d /var/lib/data-${printerName}/logs 0775 klipper klipper"
         "d /var/lib/data-${printerName}/gcodes 0775 klipper klipper"
         "d /var/lib/data-${printerName}/systemd 0775 klipper klipper"
         "d /var/lib/data-${printerName}/comms 0775 klipper klipper"
-        "C /var/lib/data-${printerName}/config/printer.cfg 0775 klipper klipper - ${configFile}/klipper/printer.cfg"
-        "C /var/lib/data-${printerName}/config/moonraker.conf 0775 moonraker moonraker - ${configFile}/moonraker/moonraker.conf"
+        "C /var/lib/data-${printerName}/config/printer.cfg 0775 klipper klipper - ${configs.klipperCfg}"
       ])
       printers);
   };
